7905f95f33948cd899b4415d926a5ae8025a4478
Emmi Quirin <emmi.quirin@forefront.se>
2025-09-30 14:53:20 +0200

    form component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import './App.css';
+import './components/FormComponent'
+import FormComponent from './components/FormComponent';
 
 function App() {
 
   return (
     <>
       <h1>Vite + React</h1>
+      <FormComponent />
     </>
   );
 }
---
+++ src/components/FormComponent.tsx
@@ -0,0 +1,12 @@
+const FormComponent = () => {
+    return (
+        <>
+            <form>
+                <input type="number" />
+                <input type="number" />
+            </form>
+        </>
+    )
+}
+
+export default FormComponent;
