cddcd7795e7900502d726afe3a5ee8d47e69b83b
Emmi Quirin <emmi.quirin@forefront.se>
2025-10-01 15:57:17 +0200

    Created round robin funtion

--- src/components/FormComponent/FormComponent.tsx
+++ src/components/FormComponent/FormComponent.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import './FormComponent.scss'
 
-
 const FormComponent = () => {
     const [participants, setParticipants] = useState<string>('');
     const [rounds, setRounds] = useState<string>('');
@@ -12,12 +11,59 @@ const FormComponent = () => {
         const n = Number(participants);
         const d = Number(rounds);
         
-        if (n % 2 === 0) {
-            console.log("Form submitted", {n, d})
+        if (n % 2 === 0 && d >= 1 && d <= n - 1) {
+            roundRobin(n, d)
         } else {
-            console.log("Praticipants must be even number")
+            console.log("Praticipants must be even number and rounds lesser than participants")
         }
     }
+
+    function roundRobin(n, d) {
+        const participants = [
+            { id: 1, name: "Alice" },
+            { id: 2, name: "Bob" },
+            { id: 3, name: "Charlie" },
+            { id: 4, name: "Diana" },
+            { id: 5, name: "Ethan" },
+            { id: 6, name: "Fiona" },
+            { id: 7, name: "George" },
+            { id: 8, name: "Hannah" },
+            { id: 9, name: "Isaac" },
+            { id: 10, name: "Julia" },
+            { id: 11, name: "Kevin" },
+            { id: 12, name: "Laura" },
+            { id: 13, name: "Michael" },
+            { id: 14, name: "Nina" },
+            { id: 15, name: "Oscar" },
+            { id: 16, name: "Paula" },
+            { id: 17, name: "Quentin" },
+            { id: 18, name: "Rachel" },
+            { id: 19, name: "Samuel" },
+            { id: 20, name: "Tina" }
+        ];
+
+        // fixera spelare [0] och rotera övriga 
+        const pairs = []
+
+        for(let r = 0; r < n - 1; r++) {
+
+            const pair = []
+
+            pair.push([0, (r % (n - 1) + 1)])
+
+            for(let i = 1; i < (n / 2); i++) {
+                const firstPlayer = (r + i) % (n - 1) + 1;
+                const secondPlayer = (r + n - i - 1) % (n - 1) + 1;
+
+                pair.push([firstPlayer, secondPlayer])
+            }
+
+            pairs.push(pair)
+        }
+
+        console.log(pairs)
+    }
+
 
     return (
         <>
@@ -30,7 +76,7 @@ const FormComponent = () => {
                     onChange={(e) => setParticipants(e.target.value)}
                     required
                 />
-                <label htmlFor="rounds">Tournament Rounds*</label>
+                <label htmlFor="rounds">Tournament Rounds (must be at least 1 and lesser than participants)*</label>
                 <input 
                     type="number" 
                     id="rounds"
